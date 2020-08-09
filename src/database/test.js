9b449adc6b528dc109b1551a77c638f2d1929c67
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-08-09 19:45:39 -0300

    Add search on db by filter

--- src/database/test.js
+++ src/database/test.js
@@ -12,7 +12,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: 'Português',
+        subject: 8,
         cost: '15',
         // O proffy id virá pelo banco de dados
     }
@@ -36,5 +36,25 @@ Database.then(async (db) => {
     // Consultar os dados
 
     // Todos os proffys
-    
+    const selectProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectProffys)
+
+    // Consultar as classes de um determinado professor e trazer junto os dados do professor
+    const selectClassesAndProffys = await db.all(`
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+    `)
+    // console.log(selectClassesAndProffys)
+
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "520"
+    `)
+    console.log(selectClassesSchedules)
 })
